e8521c9dc7415a56ec6a6ab5f4e85ec01d8960f5
Taylor Baldwin <taylorbaldwin@gmail.com>
2015-07-19 20:35:26 -0400

    removing unnecessary files

--- js/quotes.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-    'Whatever the mind of man can conceive and believe, it can achieve.',
-    'Strive not to be a success, but rather to be of value.',
-    'Two roads diverged in a wood, and I — I took the one less traveled by, And that has made all the difference.',
-    'You miss 100% of the shots you don’t take.',
-    'The most difficult thing is the decision to act, the rest is merely tenacity.',
-    'Every strike brings me closer to the next home run.',
-    'Either you run the day, or the day runs you.'
-];
